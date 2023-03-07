66042a483a85a065047561aafa15a58d22de77ea
Myeung Jin Lee <66550892+mjlee111@users.noreply.github.com>
2023-03-07 15:38:02 +0900

    Update opencv_3.4.0_install.sh

--- opencv_3.4.0_install.sh
+++ opencv_3.4.0_install.sh
@@ -1,23 +1,11 @@
 echo -e "\033[40;31m[RO:BIT] installing OpenCV 3.4.0\033[0m"
 echo -e "\033[40;31m[RO:BIT] robit 17th myeungjin lee\033[0m"
-echo ""
-echo ""
-echo ""
-
 echo -e "\033[40;31m[RO:BIT] DELETING other OpenCV\033[0m"
 sudo apt-get purge -y libopencv* python-opencv
 sudo apt autoremove -y
-echo ""
-echo ""
-echo ""
-
 echo -e "\033[40;31m[RO:BIT] updating packages\033[0m"
 sudo apt update -y
 sudo apt upgrade -y
-echo ""
-echo ""
-echo ""
-
 echo -e "\033[40;31m[RO:BIT] installing packages\033[0m"
 sudo apt-get install -y build-essential cmake unzip pkg-config
 sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
@@ -48,10 +36,6 @@ sudo apt-get install -y libatlas-base-dev gfortran libeigen3-dev
 sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y python2.7-dev python3-dev python-numpy python3-numpy
 sudo apt-get install -y qt5-default
-echo ""
-echo ""
-echo ""
-
 echo -e "\033[40;31m[RO:BIT] making OpenCV folder\033[0m"
 mkdir opencv
 cd opencv/
@@ -62,27 +46,17 @@ unzip opencv.zip && unzip opencv_contrib.zip
 cd opencv-3.4.0
 mkdir build
 cd build
-echo ""
-echo ""
-echo ""
 
 echo -e "\033[40;31m[RO:BIT] CMAKE!\033[0m"
 
-
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=OFF -D WITH_IPP=OFF -D WITH_1394=OFF -D BUILD_WITH_DEBUG_INFO=OFF -D BUILD_DOCS=OFF -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D WITH_QT=OFF -D WITH_GTK=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.0/modules -D WITH_V4L=ON  -D WITH_FFMPEG=ON -D WITH_XINE=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D PYTHON2_INCLUDE_DIR=/usr/include/python2.7 -D PYTHON2_NUMPY_INCLUDE_DIRS=/usr/lib/python2.7/dist-packages/numpy/core/include/ -D PYTHON2_PACKAGES_PATH=/usr/lib/python2.7/dist-packages -D PYTHON2_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so -D PYTHON3_INCLUDE_DIR=/usr/include/python3.6m -D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include/  -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages -D PYTHON3_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so ../
 
-echo ""
-echo ""
-echo ""
 
 echo -e "\033[40;31m[RO:BIT] make\033[0m"
 time make -j $(nproc)
 sudo make install
 sudo sh -c 'echo '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
-echo ""
-echo ""
-echo ""
 
 echo -e "\033[40;31m[RO:BIT] Installation Complete\033[0m"
 echo -e "\033[40;31m[RO:BIT] Curent version of OpenCV is : \033[0m" 
